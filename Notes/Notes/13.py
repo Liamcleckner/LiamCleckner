aa0d1ceec94e604209e6ee43a3c90c907355676b
Liamcleckner <liamcleckner@gmail.com>
2025-10-13 22:47:01 +0000

    13

---
+++ Notes/Notes/13.py
@@ -0,0 +1,25 @@
+ss = "Hello World  !"
+tt = ss.upper()
+print(tt)
+print(tt.capitalize())
+
+'''
+read_line  =
+
+sstt = "1,2,3,4,5,6,7"
+numbers = read_line.split(',')
+numbers = map(int,numbers)
+print(list(numbers))
+'''
+
+print(ss.count(' '))
+
+#For Kattis
+
+line = input()
+if('ss' in line):
+    ans = "hiss"
+else:
+    ans = "no hiss"
+
+print(ans)
