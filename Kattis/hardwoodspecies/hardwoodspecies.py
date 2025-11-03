18a0717094502f9258951361d3e40d265768c3c0
Liamcleckner <liamcleckner@gmail.com>
2025-11-03 23:22:35 +0000

    kat

---
+++ Kattis/hardwoodspecies/hardwoodspecies.py
@@ -0,0 +1,23 @@
+'''
+Programmer: Liam Cleckner
+Class: CS110
+Program: Kattis problem - 
+Algorithm:
+    input lines "trees" into a list
+        use for line in sys.stdin:
+'''
+
+
+
+import sys
+
+species = sys.stdin.readlines() # return a list of lines until BOF
+print(species[0].strip())
+
+print(species)
+
+species = []
+for line in sys.stdin:
+    species.append(line)
+
+print(species[0])
--- Notes/Notes/14.py
+++ Notes/Notes/14.py
@@ -15,3 +15,13 @@ if "CO" in empty_dict.keys():
 print(f"{empty_dict.keys()=}")
 print(f"{empty_dict.values()=}")
 print(f"{empty_dict.items()=}")
+
+empty_dict['VT'] = "Montpelier"
+
+if 'VT' in empty_dict:
+    print(f"Value at 'VT': {empty_dict['VT']}")
+
+if 'VT' in empty_dict.keys():
+    print(f"Value at 'VT': {empty_dict['VT']}")
+
+print(f"Value at 'VT': {empty_dict.get('VT', 'no se')}")
