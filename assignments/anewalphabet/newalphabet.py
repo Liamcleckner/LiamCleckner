c8e73365a1a3b0452b6fa796587ca082d1f12787
Liamcleckner <liamcleckner@gmail.com>
2025-11-23 06:43:35 +0000

    new alphabter done

--- assignments/anewalphabet/newalphabet.py
+++ assignments/anewalphabet/newalphabet.py
@@ -20,17 +20,17 @@ letters = {
     'j': '_|',
     'k': '|<',
     'l': '1',
-    'm': '[]\/[]',
-    'n': '[]\[]',
+    'm': '[]\\/[]',
+    'n': '[]\\[]',
     'o': '0',
     'p': '|D',
     'q': '(,)',
     'r': '|Z',
     's': '$',
-    't': '][',
+    't': '\'][\'',
     'u': '|_|',
-    'v': '\/',
-    'w': '\/\/',
+    'v': '\\/',
+    'w': '\\/\\/',
     'x': '}{',
     'y': '`/',
     'z': '2'
@@ -46,9 +46,16 @@ def translate(text):
             result.append(character)
     return ''.join(result)
 
+def test():
+    assert translate("cat") == "(@']['"
+    assert translate("Hello world!") == "[-]3110 \\/\\/0|Z1|)!"
+    assert translate("abc 123") == "@8( 123"
+
+
 def main():
     text = input().strip()
     print(translate(text))
 
 if __name__ == '__main__':
+    test()
     main()
