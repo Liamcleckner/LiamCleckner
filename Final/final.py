603f647644839152c9cc315292f884dbffb5e72c
Liamcleckner <liamcleckner@gmail.com>
2025-11-24 23:48:54 +0000

    final

--- Final/final.py
+++ Final/final.py
@@ -7,7 +7,7 @@ Date: 11/22/2025
 
 import random
 
-gallows = {
+gallows = [
     '''
       _______
      |       |
@@ -79,23 +79,24 @@ gallows = {
     ============
     '''
 
-}
+]
 
 max_tries = len(gallows) - 1
 
 def choose_word():
     with open("words.txt") as f:
         word = random.choice(f.read().splitlines())
+    return word.lower()
     # print("Random word:", word)
 
-def round(word):
+def play_round(word):
     wrong_guesses = 0
     guessed = set()
     
     while True:
         ascii(word, guessed, wrong_guesses)
 
-        if word_solved():
+        if word_solved(word, guessed):
             print("You won! \nSecret word: ", word)
         
         if wrong_guesses >= max_tries:
@@ -105,22 +106,22 @@ def round(word):
         if letter == "quit":
             print("Game over. \nThe word was", word)
         if letter in word:
-            guessed.addletter
+            guessed.add(letter)
             print("Good guess.", letter)
         else:
-            guessed.addletter
+            guessed.add(letter)
             wrong_guesses += 1
             print("Bad guess.", letter)
 
 def letter_input(guessed):
     while True:
-        letter_guess = input("(To leave, type quit) Guess a letter: ").strip().lower
-        if letter_guess == quit:
+        letter_guess = input("(To leave, type quit) Guess a letter: ").strip().lower()
+        if letter_guess == "quit":
             return letter_guess
         if letter_guess in guessed:
             print("Already guessed that letter!")
             continue
-        if len(letter_guess) != 1 or not letter_guess.isalpha:
+        if len(letter_guess) != 1 or not letter_guess.isalpha():
             print("Please etner one letter.")
             continue
         return letter_guess
@@ -139,13 +140,20 @@ def ascii(word, guessed, wrong_guesses):
 def play_again():
     while True:
         ans = input("Play again? (y/n)").strip().lower()[0]
+        if ans == "y":
+            return True
+        if ans == "n":
+            return False
 
 
 def main():
     print("Welcome to my hangman game!")
     while True:
         word = choose_word()
-        round(word)
+        play_round(word)
+        if not play_again():
+            print("Thanks for playing!!")
+            break
 
 if __name__ == "__main__":
     main()
