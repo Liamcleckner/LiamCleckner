cc36a297a7ed493aac10dbc693ddd36f17f33d24
Liamcleckner <liamcleckner@gmail.com>
2025-11-24 05:00:43 +0000

    OOP Finished

--- Labs/OOP/oop.py
+++ Labs/OOP/oop.py
@@ -29,28 +29,34 @@ class Circle(object):
         return math.pi*(self.radius**2)
 
     def findCircumference(self):
-        # FIXME1: Find and return the circumference of the circle (20 pts)
-        return 0
+        # fixed FIXME1: Find and return the circumference of the circle (20 pts)
+        return 2*math.pi*(self.radius)
 
     def __str__(self):
-         # FIXME2: Modify the statement to return circumference as well (20 pts)
-        return "Circle - radius: {:.2f} and area: {:.2f}".format(self.radius, self.area)
+         # fixed FIXME2: Modify the statement to return circumference as well (20 pts)
+        return "Circle - radius: {:.2f} and area: {:.2f} and circumference: {:.2f}".format(self.radius, self.area, self.circumference)
 
 
 def main():
     # instantiate aCir object with radius 1
     aCir = Circle(1)
     print(aCir)
-    # FIXME3: (20 pts)
+    # fixed FIXME3: (20 pts)
     # Instantiate another circle with radius 5
     # Print the circle
+    bCir = Circle(5)
+    print(bCir)
 
-    # FIMXE4: (20 pts)
+    # fixed FIMXE4: (20 pts)
     # prompt user to enter radius and store into a variable
-
-    # FIXME5: (20 pts)
+    cRad = int(input("Enter the radius of a circle: "))
+    cCir = Circle(cRad)
+    print(cCir)
+    # fixed FIXME5: (20 pts)
     # create a circle with the user provided radius
     # print the circle
+    cCir = Circle(cRad)
+    print(cCir)
 
 
 if __name__ == "__main__":
